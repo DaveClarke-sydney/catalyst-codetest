11ca38c37036a054270760ab5d77f87582b9a20b
David Clarke <dave.clarke.1990@gmail.com>
2020-09-25 15:33:40 +1000

    Handling input

--- user_upload.php
+++ user_upload.php
@@ -2,8 +2,110 @@
 
 require_once('./models/userModel.php');
 
-$userModel = new userModel();
+class userController
+{
+	private $validOptions = [
+		'--file', 
+		'--create_table', 
+		'--dry_run', 
+		'-u', 
+		'-p', 
+		'-h', 
+		'--help',
+	];
 
+	public function main($argc, $argv)
+	{
+		$userModel = new userModel();
+
+		try {
+			list($filename, $options) = $this->parseInput($argc, $argv);
+		} catch (Exception $e) {
+			echo $e->getMessage()."\n";
+			return;
+		}
+
+		if(in_array('--help', $options))
+		{
+			$this->displayHelp();	
+		} elseif(in_array('-u', $options))
+		{
+			$this->displayUser();
+		} elseif(in_array('-p', $options))
+		{
+			$this->displayPassword();
+		} elseif(in_array('-h', $options))
+		{
+			$this->displayHost();
+		} elseif(in_array('--create_table', $options))
+		{
+			$this->createTableOnly();
+		} else {
+			$dry_run = in_array('--dry_run', $options);
+			$this->processFile($filename, $dry_run);
+		}
+	}
+
+	public function parseInput($argc, $argv)
+	{
+		$options = [];
+		$filename = null;
+		for ($i=1; $i < $argc; $i++) {
+			if($argv[$i][0] == '-'){
+				if($argv[$i] == '--file'){
+					$i++;
+					$filename = $argv[$i];
+				} elseif(in_array($argv[$i], $this->validOptions)){
+    					$options[] = $argv[$i];
+				} else {
+					throw new Exception("unrecognized option '{$argv[$i]}'");
+				}
+			} else {
+				throw new Exception("malformed arguments");
+			}
+		}
+		return [$filename, $options];
+	}
+
+	public function displayHelp()
+	{
+		echo "displaying Help\n";
+	}
+
+	public function displayUser()
+	{
+		echo "displaying User\n";
+	}
+
+	public function displayPassword()
+	{
+		echo "displaying Password\n";
+	}
+
+	public function displayHost()
+	{
+		echo "displaying Host\n";
+	}
+
+	public function createDatabaseOnly()
+	{
+		echo "creating Database only\n";
+	}	
+
+	public function processFile($filename)
+	{
+		if(substr($filename, -4) != '.csv'){
+			throw new Exception('not a .csv file');
+		}
+		echo "processing file $filename\n";
+	}
+}
+
+(new userController())->main($argc, $argv);
+
+
+
+/*
 $records = [
 	['name' => 'John', 'surname' => 'Smith', 'email' => 'john@smith.com'],
 	['name' => 'Sherlock', 'surname' => 'Holmes', 'email' => 'sherlock@holmes.com'],
@@ -18,5 +120,6 @@ if ($result->num_rows > 0) {
 } else {
   echo "0 results";
 }
+*/
 
 ?>
