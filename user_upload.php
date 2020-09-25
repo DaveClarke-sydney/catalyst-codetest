3952464222abf2cdc7e2c107b650b1ec4d116aef
David Clarke <dave.clarke.1990@gmail.com>
2020-09-25 12:44:37 +1000

    Connection to database established

--- user_upload.php
+++ user_upload.php
@@ -1,8 +1,23 @@
 <?php
 
+$servername = "localhost";
+$username = "testuser";
+$password = "password";
+$dbname = "catalyst";
+
+// Create connection
+$conn = new mysqli($servername, $username, $password, $dbname);
+
+// Check connection
+if ($conn->connect_error) {
+  die("Connection failed: " . $conn->connect_error);
+}
+echo "Connected successfully\n";
 
 
-echo "hello world";
+$sql = "SELECT * FROM users";
+$result = $conn->query($sql);
 
+var_dump($result);
 
 ?>
